4840c072c7acb50b277b54e2a6cefe01c84ebf54
mACIEK0321 <mkolakowski75@gmail.com>
2026-02-23 20:42:17 +0100

    k

--- monte-nextjs/src/components/AboutUs.tsx
+++ monte-nextjs/src/components/AboutUs.tsx
@@ -90,7 +90,7 @@ export default function AboutUs() {
                     className="team-photo-img"
                     width={300}
                     height={300}
-                    style={{ objectFit: 'cover', borderRadius: '50%', width: '100%', height: '100%' }}
+                    style={{ objectFit: 'cover', width: '100%', height: '100%' }}
                   />
                 </div>
                 <div className="team-content">
@@ -122,7 +122,7 @@ export default function AboutUs() {
                     className="team-photo-img"
                     width={300}
                     height={300}
-                    style={{ objectFit: 'cover', borderRadius: '50%', width: '100%', height: '100%' }}
+                    style={{ objectFit: 'cover', width: '100%', height: '100%' }}
                   />
                 </div>
                 <div className="team-content">
@@ -149,22 +149,45 @@ export default function AboutUs() {
           <div className="trust-badges-grid">
             <div className="trust-badge">
               <div className="trust-badge-icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+                </svg>
+              </div>
+              <p className="trust-badge-text">
+                Certyfikat MF nr 36393/2020 - uprawnienie do prowadzenia ksiąg
+              </p>
+            </div>
+
+            <div className="trust-badge">
+              <div className="trust-badge-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
               </div>
               <p className="trust-badge-text">
                 Ponad 20 lat doświadczenia
+              </p>
+            </div>
+
+            <div className="trust-badge">
+              <div className="trust-badge-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                </svg>
+              </div>
+              <p className="trust-badge-text">
+                Stała współpraca z doradcą podatkowym
+              </p>
+            </div>
+
+            <div className="trust-badge">
+              <div className="trust-badge-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+                </svg>
+              </div>
+              <p className="trust-badge-text">
+                Praca zgodna z aktualnymi przepisami
               </p>
             </div>
           </div>
