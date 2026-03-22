882afc0f66ed89eb6d14aee1e292d93c8f42ce47
mACIEK0321 <mkolakowski75@gmail.com>
2026-03-22 06:51:05 +0100

    poprawka2

--- monte-nextjs/src/app/(pl)/blog/[slug]/page.tsx
+++ monte-nextjs/src/app/(pl)/blog/[slug]/page.tsx
@@ -178,7 +178,7 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
           <div className="blog-article-hero-image">
             <Image
               src={imageUrl!}
-              alt={post.mainImage?.alt || post.title}
+              alt={post!.mainImage?.alt || post!.title}
               width={1200}
               height={600}
               priority
